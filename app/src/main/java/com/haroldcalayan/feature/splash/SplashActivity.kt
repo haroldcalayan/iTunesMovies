fa1856b668ed133f8d7ae78ac30515eb1c148faf
Harold Calayan <haroldcalayan@gmail.com>
2020-11-12 23:21:37 +0800

    :sparkles: Create Splash screen

---
+++ app/src/main/java/com/haroldcalayan/feature/splash/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.haroldcalayan.feature.splash
+
+import android.content.Intent
+import com.haroldcalayan.BR
+import com.haroldcalayan.R
+import com.haroldcalayan.common.base.BaseActivity
+import com.haroldcalayan.databinding.ActivitySplashBinding
+import com.haroldcalayan.feature.master.MasterActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun getLayout() = R.layout.activity_splash
+
+    override fun getBindingVariable() = BR.viewModel
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
+    override fun initViews() {
+        super.initViews()
+        hideSystemUI()
+        //startTimer()
+    }
+
+    private fun startTimer() {
+        activityScope.launch {
+            delay(SPLASH_SCREEN_LIFE)
+            openMaster()
+            finish()
+        }
+    }
+
+    private fun openMaster() {
+        Intent(this, MasterActivity::class.java).apply {
+            startActivity(this)
+        }
+    }
+
+    companion object {
+        private const val SPLASH_SCREEN_LIFE = 3000L
+    }
+}
---
+++ app/src/main/java/com/haroldcalayan/feature/splash/SplashViewModel.kt
@@ -0,0 +1,32 @@
+package com.haroldcalayan.feature.splash
+
+import androidx.lifecycle.viewModelScope
+import com.haroldcalayan.common.base.BaseViewModel
+import com.haroldcalayan.data.MovieRepository
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+class SplashViewModel : BaseViewModel() {
+
+    @Inject
+    lateinit var movieRepository: MovieRepository
+
+    init {
+        getMovies()
+    }
+
+    private fun getMovies() {
+        viewModelScope.launch {
+            val request = runCatching { movieRepository.getAllItems() }
+            request.apply {
+                onSuccess {
+                    Timber.d("movies: $it")
+                }
+                onFailure {
+                    it.printStackTrace()
+                }
+            }
+        }
+    }
+}
