c6bb30da882911e2026ec003a2611bd345f14806
Harold Calayan <haroldcalayan@gmail.com>
2020-11-13 00:07:13 +0800

    :sparkles: Apply dependency injection in master and detail viewmodels

--- app/src/main/java/com/haroldcalayan/common/di/component/AppComponent.kt
+++ app/src/main/java/com/haroldcalayan/common/di/component/AppComponent.kt
@@ -4,7 +4,8 @@ import com.haroldcalayan.common.di.module.AppModule
 import com.haroldcalayan.common.di.module.NetworkModule
 import com.haroldcalayan.common.di.module.RepositoryModule
 import com.haroldcalayan.data.MovieRepository
-import com.haroldcalayan.data.MovieRepositoryImpl
+import com.haroldcalayan.feature.detail.DetailViewModel
+import com.haroldcalayan.feature.master.MasterViewModel
 import com.haroldcalayan.feature.splash.SplashViewModel
 import dagger.Component
 import javax.inject.Singleton
@@ -14,6 +15,8 @@ import javax.inject.Singleton
 interface AppComponent {
 
     fun inject(viewModel: SplashViewModel)
+    fun inject(viewModel: MasterViewModel)
+    fun inject(viewModel: DetailViewModel)
 
     fun inject(repository: MovieRepository)
 }
