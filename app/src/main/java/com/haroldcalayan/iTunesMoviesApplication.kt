040bb997ddbcf0a4189c107a81b1dd7f19b69973
Harold Calayan <haroldcalayan@gmail.com>
2020-11-12 23:21:08 +0800

    :sparkles: Create Application class

--- app/src/main/java/com/haroldcalayan/iTunesMoviesApplication.kt
+++ app/src/main/java/com/haroldcalayan/iTunesMoviesApplication.kt
@@ -1,6 +1,34 @@
 package com.haroldcalayan
 
 import android.app.Application
+import com.haroldcalayan.common.di.component.AppComponent
+import com.haroldcalayan.common.di.component.DaggerAppComponent
+import com.haroldcalayan.common.di.module.AppModule
+import com.haroldcalayan.common.di.module.NetworkModule
+import com.haroldcalayan.common.di.module.RepositoryModule
+import timber.log.Timber
 
-class iTunesMoviesApplication : Application() {
+class ITunesMoviesApplication : Application() {
+
+    val appComponent: AppComponent by lazy { initializeComponent() }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        initLog()
+    }
+
+    private fun initializeComponent() =  DaggerAppComponent.builder()
+        .appModule(AppModule(this))
+        .networkModule(NetworkModule(BuildConfig.BASE_APP_URL))
+        .repositoryModule(RepositoryModule())
+        .build()
+
+    private fun initLog() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    companion object {
+        lateinit var instance: ITunesMoviesApplication
+    }
 }
