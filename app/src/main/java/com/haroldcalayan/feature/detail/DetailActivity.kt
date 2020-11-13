8f977d02715759c4570815155c29d8a94df7e170
Harold Calayan <haroldcalayan@yahoo.com>
2020-11-13 13:31:43 +0800

    :pencil: Update fab message

--- app/src/main/java/com/haroldcalayan/feature/detail/DetailActivity.kt
+++ app/src/main/java/com/haroldcalayan/feature/detail/DetailActivity.kt
@@ -28,7 +28,7 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>() {
     setSupportActionBar(findViewById(R.id.detail_toolbar))
 
     findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
+      Snackbar.make(view, "This feature will be available soon.", Snackbar.LENGTH_LONG)
         .setAction("Action", null)
         .show()
     }
