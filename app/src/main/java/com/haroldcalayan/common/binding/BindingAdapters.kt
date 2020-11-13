fadc3f7b3c8aeb40d5125e4edd3255925de8ca27
Harold Calayan <haroldcalayan@gmail.com>
2020-11-13 08:58:05 +0800

    :sparkles: Create BindingAdapters

---
+++ app/src/main/java/com/haroldcalayan/common/binding/BindingAdapters.kt
@@ -0,0 +1,11 @@
+package com.haroldcalayan.common.binding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.haroldcalayan.R
+import com.squareup.picasso.Picasso
+
+@BindingAdapter("app:image")
+fun setImage(view: ImageView, img: String?) {
+    Picasso.get().load(img).placeholder(R.drawable.movies_logo).into(view)
+}
